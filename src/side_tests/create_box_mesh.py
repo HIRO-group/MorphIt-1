c7ce45b498cfff48c5af8879e510186734fd6eb6
Nataliya <23035717+nataliya-dev@users.noreply.github.com>
2026-01-11 23:28:11 -0700

    renaming

--- src/side_tests/create_box_mesh.py
+++ src/side_tests/create_box_mesh.py
@@ -1,28 +1,15 @@
 import trimesh
-
-# Box dimensions [width, depth, height]
 box_size = [0.05, 0.05, 0.05]
-
-# Create the box
 mesh = trimesh.creation.box(extents=box_size)
-
-rho = 4.0  # or whatever density you want
+rho = 4.0
 mesh.apply_translation(-mesh.center_mass)  # move COM to (0,0,0)
-
-
 V_mesh = mesh.volume
 M_target = rho * V_mesh
-COM_target = mesh.center_mass  # shape (3,)
-
-# moment of inertia about some origin; be consistent later
-I_target = mesh.moment_inertia  # 3x3
+COM_target = mesh.center_mass
+I_target = mesh.moment_inertia
 print("Target mass:", M_target)
 print("Target COM:", COM_target)
 print("Target inertia:\n", I_target)
-
-
-# Save the box
-output_path = "../../mesh_models/box.obj"  # Change this to your desired path
+output_path = "../../mesh_models/box.obj"
 mesh.export(output_path)
-
 print(f"Box saved to: {output_path}")
